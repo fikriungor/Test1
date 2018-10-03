18f3aec6cfa3031756cb90b22c80808e9f24c500
fikriungor <fikriungor@gmail.com>
2018-10-02 20:08:17 -0400

    selenium project

---
+++ src/test/java/com/AtlantaScienceAcademy/app/RestAPITest.java
@@ -0,0 +1,20 @@
+package com.AtlantaScienceAcademy.app;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void vPos()
+	{
+		System.out.println("vPos");
+	}
+	
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
